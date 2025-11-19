d5f95eb8bf26ed6c33cd35b87e8ff782be19789d
lihua00120 <symbalily@gmail.com>
2025-11-20 00:01:36 +0800

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -7,7 +7,7 @@ import os
 
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, CarouselTemplate, CarouselColumn, TemplateSendMessage
+from linebot.models import MessageEvent,MessageAction, TextMessage, TextSendMessage, CarouselTemplate, CarouselColumn, TemplateSendMessage
 
 
 # ============================
